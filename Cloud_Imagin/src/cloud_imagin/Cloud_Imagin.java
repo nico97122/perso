e698dda9564405f858e41c99d3be4f99748cfbf6
nicolas97122 <nictyn971@gmail.com>
2019-01-24 09:50:01 +0100

    Modifier main

--- Cloud_Imagin/src/cloud_imagin/Cloud_Imagin.java
+++ Cloud_Imagin/src/cloud_imagin/Cloud_Imagin.java
@@ -15,7 +15,8 @@ public class Cloud_Imagin {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO codeint  application logic here
+        int mabite;
     }
     
 }
